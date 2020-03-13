31c75dc511152c4cf4b84d41afab777034c66640
propene123 <benjamin.russell@durham.ac.uk>
2020-03-13 07:53:29 +0000

    submit 1

---
+++ submit/backend.py
@@ -0,0 +1,264 @@
+import sys
+import copy
+import Pyro4
+import Pyro4.errors
+import Pyro4.configuration
+
+# ensure requests are dealt with sequentially (message ordering)
+Pyro4.config.SERVERTYPE = 'multiplex'
+
+NAME = ''
+
+
+@Pyro4.behavior(instance_mode='single')
+class Backend():
+    def __init__(self):
+        self._name = NAME
+        self._backups = []
+        self._clientID = 1
+        self._responses = {}
+        self._orders = {}
+        self._db = [
+            ['General Store', [['Potato', 0.24, 300],
+                               ['Carrot', 0.05, 5000],
+                               ['Pizza', 4, 50],
+                               ['Cake', 7, 100],
+                               ['Loaf of Bread', 0.5, 10000]]],
+            ['Secret Shop', [['Mango', 0.85, 1000],
+                             ['Enchanted Mango', 500, 2],
+                             ['Magic Beans', 200, 3],
+                             ['Dragon Fruit', 2, 300]]]
+        ]
+
+    def find_backups(self):
+        try:
+            with Pyro4.locateNS() as ns:
+                for backup, backup_uri in ns.list(prefix='justHungry.backend.').items():
+                    if backup != f'justHungry.backend.{self._name}':
+                        backup_proxy = Pyro4.Proxy(backup_uri)
+                        exists = False
+                        for entry in self._backups:
+                            if backup == entry[0]:
+                                exists = True
+                        if exists:
+                            continue
+                        try:
+                            res = backup_proxy.initBackup(self._db, self._responses, self._orders, self._clientID)
+                            if not res:
+                                try:
+                                    with Pyro4.locateNS() as ns:
+                                        ns.remove(name=backup)
+                                except Pyro4.errors.NamingError:
+                                    pass
+                            else:
+                                self._backups.append([backup, backup_proxy])
+                        except Pyro4.errors.PyroError:
+                            try:
+                                with Pyro4.locateNS() as ns:
+                                    ns.remove(name=backup)
+                            except Pyro4.errors.NamingError:
+                                pass
+        except Pyro4.errors.NamingError:
+            pass
+
+    @Pyro4.expose
+    def initBackup(self, db, resp, new_orders, new_client_id):
+        self._responses = copy.deepcopy(resp)
+        self._db = db[::]
+        self._orders = copy.deepcopy(new_orders)
+        self._clientID = new_client_id
+        return True
+
+    @Pyro4.expose
+    def propogate(self, u_id, resp, new_db, new_orders, new_client_id):
+        self._responses[u_id] = resp
+        self._db = new_db[::]
+        self._orders = copy.deepcopy(new_orders)
+        self._clientID = new_client_id
+        return True
+
+    @Pyro4.expose
+    def getItem(self, store, order_item, u_id):
+        self.find_backups()
+        if u_id in self._responses:
+            item = self._responses[u_id]
+        else:
+            try:
+                item = self._db[store][1][order_item]
+            except:
+                item = []
+            self._responses[u_id] = item
+        return item
+
+
+    @Pyro4.expose
+    def getOrders(self, client_id, u_id):
+        self.find_backups()
+        if u_id in self._responses:
+            orders = self._responses[u_id]
+        else:
+            try:
+                orders = self._orders[client_id]
+            except:
+                orders = []
+            self._responses[u_id] = orders
+        return orders
+
+
+    @Pyro4.expose
+    def getItemName(self, store, order_item, u_id):
+        self.find_backups()
+        if u_id in self._responses:
+            item_name = self._responses[u_id]
+        else:
+            try:
+                item_name = self._db[store][1][order_item][0]
+            except:
+                item_name = ''
+            self._responses[u_id] = item_name
+        return item_name
+
+    @Pyro4.expose
+    def getItems(self, store, u_id):
+        self.find_backups()
+        if u_id in self._responses:
+            items = self._responses[u_id]
+        else:
+            try:
+                items = self._db[store][1]
+            except:
+                items = ['ERROR']
+            self._responses[u_id] = items
+        return items
+
+    @Pyro4.expose
+    def getStoreName(self, store, u_id):
+        self.find_backups()
+        if u_id in self._responses:
+            store_name = self._responses[u_id]
+        else:
+            try:
+                store_name = self._db[store][0]
+            except:
+                store_name = ''
+            self._responses[u_id] = store_name
+        return store_name
+
+    @Pyro4.expose
+    def getStores(self, u_id):
+        self.find_backups()
+        if u_id in self._responses:
+            stores = self._responses[u_id]
+        else:
+            stores = []
+            for store in self._db:
+                stores.append(store[0])
+            self._responses[u_id] = stores
+        return stores
+
+    @Pyro4.expose
+    def placeOrder(self, store, order_item, quant, u_id):
+        self.find_backups()
+        if u_id in self._responses:
+            valid = self._responses[u_id]
+        else:
+            valid = (0 < quant <= self._db[store][1][order_item][2])
+            self._responses[u_id] = valid
+        return valid
+
+    @Pyro4.expose
+    def finaliseOrder(self, store, order_item, quant, address, client_id, u_id):
+        self.find_backups()
+        resp = False
+        if u_id in self._responses:
+            resp = self._responses[u_id]
+        else:
+            resp = (0 < quant <= self._db[store][1][order_item][2])
+            if not resp:
+                return False
+            self._responses[u_id] = resp
+            self._db[store][1][order_item][2] -= quant
+            if client_id not in self._orders:
+                self._orders[client_id] = []
+            self._orders[client_id].append(
+                [self._db[store][0], self._db[store][1][order_item][0], quant, quant*self._db[store][1][order_item][1], address])
+            for backup in self._backups[::]:
+                try:
+                    res = backup[1].propogate(
+                        u_id, client_id, self._db, self._orders, self._clientID)
+                    if not res:
+                        try:
+                            with Pyro4.locateNS() as ns:
+                                ns.remove(name=backup[0])
+                                self._backups.remove(backup)
+                        except Pyro4.errors.NamingError:
+                            self._db[store][1][order_item][2] += quant
+                            self._responses[u_id] = False
+                            self._orders[client_id].pop()
+                            return False
+                except Pyro4.errors.PyroError:
+                    try:
+                        with Pyro4.locateNS() as ns:
+                            ns.remove(name=backup[0])
+                            self._backups.remove(backup)
+                    except Pyro4.errors.NamingError:
+                        self._db[store][1][order_item][2] += quant
+                        self._responses[u_id] = False
+                        self._orders[client_id].pop()
+                        return False
+        return resp
+
+    @Pyro4.expose
+    def clientId(self, u_id):
+        self.find_backups()
+        client_id = None
+        if u_id in self._responses:
+            client_id = self._responses[u_id]
+        else:
+            client_id = self._clientID
+            self._clientID += 1
+            self._responses[u_id] = client_id
+            for backup in self._backups[::]:
+                try:
+                    res = backup[1].propogate(
+                        u_id, client_id, self._db, self._orders, self._clientID)
+                    if not res:
+                        try:
+                            with Pyro4.locateNS() as ns:
+                                ns.remove(name=backup[0])
+                                self._backups.remove(backup)
+                        except Pyro4.errors.NamingError:
+                            self._clientID -= 1
+                            self._responses[u_id] = None
+                            return None
+                except Pyro4.errors.PyroError:
+                    try:
+                        with Pyro4.locateNS() as ns:
+                            ns.remove(name=backup[0])
+                            self._backups.remove(backup)
+                    except Pyro4.errors.NamingError:
+                        self._clientID -= 1
+                        self._responses[u_id] = None
+                        return None
+        return client_id
+
+    @Pyro4.expose
+    def notifyPrimary(self):
+        return True
+
+
+if len(sys.argv) < 2 or len(sys.argv) > 2:
+    sys.exit('backend should be called with 1 argument, a unique id for the server')
+NAME = sys.argv[1]
+
+try:
+    with Pyro4.Daemon() as daemon:
+        backend_uri = daemon.register(Backend)
+        try:
+            with Pyro4.locateNS() as ns:
+                ns.register('justHungry.backend.'+NAME, backend_uri)
+        except Pyro4.errors.NamingError:
+            sys.exit('Could not find nameserver. EXITING')
+        daemon.requestLoop()
+except Pyro4.errors.DaemonError:
+    sys.exit('Pyro daemon has crashed. EXITING')
---
+++ submit/client.py
@@ -0,0 +1,251 @@
+import sys
+import Pyro4
+import Pyro4.errors
+
+
+CLIENT_ID = 0
+
+try:
+    FRONTEND = Pyro4.Proxy('PYRONAME:justHungry.frontend')
+except Pyro4.errors.PyroError:
+    sys.exit('Could not connect to frontend server')
+
+
+def select_store():
+    stores = FRONTEND.getStores()
+    print('Please select one of the stores, or type quit to go back')
+    if stores == ['ERROR']:
+        print('Cannot get stores from server')
+        return -1
+    for count, store in enumerate(stores, 1):
+        print(count, store, sep='. ')
+    while True:
+        resp = input()
+        try:
+            if 0 < int(resp) <= len(stores):
+                return int(resp)
+        except ValueError:
+            pass
+        if resp.lower() == 'quit':
+            return -1
+        print('That was not a valid option please try again')
+
+
+def select_item(store):
+    items = FRONTEND.getItems(store)
+    print(f'Thank you for selecting {FRONTEND.getStoreName(store-1)}.' +
+          f' Please select an item to order or type quit to go back')
+    if items == ['ERROR']:
+        print('Cannot get items from server')
+        return -1
+    for count, item in enumerate(items, 1):
+        print(f'{count}. {item[0]}\t£{item[1]}\tstock:{item[2]}')
+    while True:
+        resp = input()
+        try:
+            if 0 < int(resp) <= len(items):
+                return int(resp)
+        except ValueError:
+            pass
+        if resp.lower() == 'quit':
+            return -1
+        print('That was not a valid option please try again')
+
+
+def select_quantity(store, order_item):
+    item = FRONTEND.getItem(store, order_item)
+    print(f'Thank you for selecting ' +
+          f'{FRONTEND.getItemName(store, order_item)}.' +
+          f' Please enter how much you would like to order or ' +
+          f'type quit to go back')
+    if not item:
+        print('Cannot get item from server')
+        return -1
+    current_stock = item[2]
+    if current_stock == 0:
+        print('That item is currently unavailable')
+        return -1
+    while True:
+        resp = input()
+        try:
+            if 0 < int(resp) <= current_stock:
+                return int(resp)
+        except ValueError:
+            pass
+        if resp.lower() == 'quit':
+            return -1
+        print('That is not a valid stock option please try again')
+
+
+def place_order(store, order_item, quant):
+    return FRONTEND.placeOrder(store, order_item, quant)
+
+
+def submit_postcode():
+    print('Please enter a postcode for the intended delivery location')
+    postcode = input()
+    return FRONTEND.getAddress(postcode)
+
+
+def house_number():
+    while True:
+        resp = input()
+        try:
+            num = int(resp)
+            return num
+        except ValueError:
+            print('That is not a number, please try again')
+
+
+def street_name():
+    return input()
+
+
+def verify_address(address):
+    print(f'You entered:\n{address}\n\nIs this the correct address y/n')
+    while True:
+        response = input()
+        if response.lower() == 'y':
+            return True
+        if response.lower() == 'n':
+            return False
+        print('That was not a correct response to the question enter y or n')
+
+
+def confirm_order(store, order_item, quant, address):
+    item = FRONTEND.getItem(store, order_item)
+    total_cost = item[1] * quant
+    print(f'You are ordering {quant} {item[0]} from ' +
+          f'{FRONTEND.getStoreName(store)} for a total cost of ' +
+          f'£{total_cost} and having it delivered to:\n{address}\n\n' +
+          f' is this correct y/n')
+    while True:
+        response = input()
+        if response.lower() == 'y':
+            return True
+        if response.lower() == 'n':
+            return False
+        print('That was not a correct response to the question enter y or n')
+
+
+def finalise_order(store, order_item, quant, address):
+    return FRONTEND.finaliseOrder(store, order_item, quant, address, CLIENT_ID)
+
+
+def flow_postcode(store, order_item, quant):
+    while True:
+        address = submit_postcode()  # add api failed option
+        if not address:
+            print('Invalid postcode entered')
+            return False
+        print(f'Please enter building number for location:\n{address}')
+        number = house_number()
+        print(f'Please enter street name for location:\n{address}')
+        street = street_name()
+        address = f'{number} {street}\n{address}'
+        correct_address = verify_address(address)  # add incorrect option
+        if not correct_address:
+            continue
+        confirm = confirm_order(store, order_item,
+                                quant, address)  # add no option
+        if not confirm:
+            print('You may redo your order')
+            return False
+        success = finalise_order(store, order_item,
+                                 quant, address)  # add failed option
+        print('Your order will now be finalised')
+        if not success:
+            print('There was an error placing your order please try again')
+            return False
+        print('Your order has been successfully placed. Your goods will arrive' +
+              ' neither early nor late but exactly when the mean to')
+        return True
+
+
+def flow_quant(store, order_item):
+    while True:
+        quant = select_quantity(store, order_item)  # add back option
+        if quant == -1:
+            return False
+        print('Your order will now be placed')
+        success = place_order(store, order_item, quant)
+        if not success:
+            print('There was a problem with your order, you may try again')
+            continue
+        res = flow_postcode(store, order_item, quant)
+        if not res:
+            continue
+        return True
+
+
+def flow_order_item(store):
+    while True:
+        order_item = select_item(store)  # add back option
+        if order_item == -1:
+            return False
+        res = flow_quant(store, order_item-1)
+        if not res:
+            continue
+        return True
+
+
+def flow_store(store):
+    while True:
+        res = flow_order_item(store)
+        if not res:
+            return False
+        return True
+
+
+def order():
+    while True:
+        store = select_store()
+        if store == -1:
+            break
+        res = flow_store(store-1)
+        if not res:
+            continue
+        break
+
+
+def view():
+    orders = FRONTEND.getOrders(CLIENT_ID)
+    if orders == ['ERROR']:
+        print('There was an error retrieving your orders. Please try again')
+    print('Here are your orders:')
+    for data in orders:
+        print(f'Store: {data[0]}')
+        print(f'Item: {data[1]}')
+        print(f'Quantity: {data[2]}')
+        print(f'Total Cost: {data[3]}')
+        print('Address:')
+        print(data[4])
+        print('##############################################################################')
+
+
+def main():
+    global CLIENT_ID
+    CLIENT_ID = FRONTEND.getClientID()
+    if not CLIENT_ID:
+        sys.exit('Cannot connect to the Just Hungry store. Exiting client')
+    print('Welcome to the Just Hungry store. Here you can order various' +
+          ' foodstuffs to any location you want.')
+    while True:
+        print('Would you like to:\n1. Make an order\n2. View your orders\n3. Quit')
+        answer = input()
+        if answer == '1':
+            order()
+        elif answer == '2':
+            view()
+        elif answer.lower() in ['quit', '3']:
+            print('Thank you for doing business with us today')
+            break
+        else:
+            print('Not a valid option. Try again')
+    # Loopify
+
+
+try:
+    main()
+except Pyro4.errors.PyroError:
+    sys.exit('Cannot connect to the Just Hungry Store. Exiting client')
